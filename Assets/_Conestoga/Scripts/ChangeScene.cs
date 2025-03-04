93b321982604337386b59970942eee4e6e78438c
Andreea Pop <apop1837@conestogac.on.ca>
2025-03-04 16:50:07 -0500

    Add ability to reset scene from button, barebone mechanics for opening door and flashlight, and some colliders to environment

--- Assets/_Conestoga/Scripts/ChangeScene.cs
+++ Assets/_Conestoga/Scripts/ChangeScene.cs
@@ -5,7 +5,22 @@ using UnityEngine.SceneManagement;
 
 public class ChangeScene : MonoBehaviour
 {
+
+    private void Start() => Fader.FadeIn();
+
     public void ChangeToSpecificScene(string sceneName)
+    {
+        StartCoroutine(FadeSceneChange(sceneName));
+        //SceneManager.LoadScene(sceneName);
+    }
+
+    public IEnumerator FadeSceneChange(string sceneName)
+    {
+        Fader.FadeOut();
+        while (Fader.isFading) yield return null; // wait until fade is complete
+        SceneManager.LoadScene(sceneName);
+    }
+    public void ChangeToSpecificSceneNonFade(string sceneName)
     {
         SceneManager.LoadScene(sceneName);
     }
